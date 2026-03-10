acb5048f8228d4726579d8889c918bcf1080f37f
irolram <ivanr9016@gmail.com>
2026-03-10 10:52:52 +0100

    avance

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ description = "Demo project for Spring Boot"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17	}
 }
 
 repositories {
