efb47d2d73bda0da150944231ffbbd926797171b
irolram <ivanr9016@gmail.com>
2026-03-16 11:01:27 +0100

    xd

--- src/main/kotlin/com/example/TfgApiSpringBoot/model/Huerto.kt
+++ src/main/kotlin/com/example/TfgApiSpringBoot/model/Huerto.kt
@@ -9,8 +9,11 @@ data class Huerto(
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     val id: String? = null,
-    val nombre: String,
-    val descripcion: String,
+
+    // 🔌 AÑADE EL = "" A ESTOS DOS PARA EVITAR EL ERROR DE JACKSON
+    val nombre: String = "",
+    val descripcion: String = "",
+
     val latitud: Double = 0.0,
     val longitud: Double = 0.0,
     val imagenUrl: String = "",
