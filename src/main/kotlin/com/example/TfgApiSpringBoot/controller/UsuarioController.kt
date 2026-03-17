f826802f32b99e02dd938bb07dce7abcf3b7a961
irolram <ivanr9016@gmail.com>
2026-03-17 12:19:12 +0100

    Update

--- src/main/kotlin/com/example/TfgApiSpringBoot/controller/UsuarioController.kt
+++ src/main/kotlin/com/example/TfgApiSpringBoot/controller/UsuarioController.kt
@@ -1,7 +1,7 @@
 package com.example.TfgApiSpringBoot.controller
 
 
-import com.example.TfgApiSpringBoot.model.Usuario
+import com.example.TfgApiSpringBoot.model.UsuarioEntity
 import com.example.TfgApiSpringBoot.repository.UsuarioRepository
 import org.springframework.web.bind.annotation.*
 
@@ -10,17 +10,17 @@ import org.springframework.web.bind.annotation.*
 class UsuarioController(private val usuarioRepository: UsuarioRepository) {
 
     @PostMapping // Esto responde al @POST de tu Retrofit en Android
-    fun registrarUsuario(@RequestBody usuario: Usuario): Usuario {
+    fun registrarUsuario(@RequestBody usuario: UsuarioEntity): UsuarioEntity {
         // Guardamos el usuario que viene de la App directamente en MySQL
         return usuarioRepository.save(usuario)
     }
 
     @GetMapping("/{id}")
-    fun obtenerUsuario(@PathVariable id: String): Usuario? {
+    fun obtenerUsuario(@PathVariable id: String): UsuarioEntity? {
         return usuarioRepository.findById(id).orElse(null)
     }
     @GetMapping
-    fun listarUsuarios(): List<Usuario> {
+    fun listarUsuarios(): List<UsuarioEntity> {
         return usuarioRepository.findAll()
     }
 }
--- src/main/kotlin/com/example/TfgApiSpringBoot/repository/IUsuarioRepository.kt
+++ src/main/kotlin/com/example/TfgApiSpringBoot/repository/IUsuarioRepository.kt
@@ -3,29 +3,29 @@ package com.example.TfgApiSpringBoot.repository
 
 
 import com.example.TfgApiSpringBoot.model.Rol
-import com.example.TfgApiSpringBoot.model.Usuario
+import com.example.TfgApiSpringBoot.model.UsuarioEntity
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.Optional
 
 @Repository
-interface UsuarioRepository : JpaRepository<Usuario, String> {
+interface UsuarioRepository : JpaRepository<UsuarioEntity, String> {
 
     // 1. Buscar por email (útil para validaciones de registro)
-    fun findByEmail(email: String): Optional<Usuario>
+    fun findByEmail(email: String): Optional<UsuarioEntity>
 
     // 2. Comprobar si un email ya existe (devuelve boolean, muy rápido)
     fun existsByEmail(email: String): Boolean
 
     // 3. Buscar todos los usuarios que tengan un rol específico (ej: todos los ADMIN)
-    fun findByRol(rol: Rol): List<Usuario>
+    fun findByRol(rol: Rol): List<UsuarioEntity>
 
     // 4. Buscar por nombre o apellido (por si quieres hacer un buscador en el panel admin)
     fun findByNombreContainingIgnoreCaseOrApellidosContainingIgnoreCase(
         nombre: String,
         apellidos: String
-    ): List<Usuario>
+    ): List<UsuarioEntity>
 
     // 5. Contar cuántos usuarios hay registrados (para estadísticas del TFG)
     override fun count(): Long
