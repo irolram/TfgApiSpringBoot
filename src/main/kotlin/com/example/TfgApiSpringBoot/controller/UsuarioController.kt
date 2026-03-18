989d9649b005b796ccb9a32bfe3a7907f4a3e848
irolram <ivanr9016@gmail.com>
2026-03-18 12:41:36 +0100

    Mejora actualizar usuario

--- src/main/kotlin/com/example/TfgApiSpringBoot/controller/UsuarioController.kt
+++ src/main/kotlin/com/example/TfgApiSpringBoot/controller/UsuarioController.kt
@@ -3,10 +3,11 @@ package com.example.TfgApiSpringBoot.controller
 
 import com.example.TfgApiSpringBoot.model.UsuarioEntity
 import com.example.TfgApiSpringBoot.repository.IUsuarioRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/usuarios") // Esta es la ruta base
+@RequestMapping("/api/usuarios") // Esta es la ruta base
 class UsuarioController(private val IUsuarioRepository: IUsuarioRepository) {
 
     @PostMapping // Esto responde al @POST de tu Retrofit en Android
@@ -19,8 +20,29 @@ class UsuarioController(private val IUsuarioRepository: IUsuarioRepository) {
     fun obtenerUsuario(@PathVariable id: String): UsuarioEntity? {
         return IUsuarioRepository.findById(id).orElse(null)
     }
+
     @GetMapping
     fun listarUsuarios(): List<UsuarioEntity> {
         return IUsuarioRepository.findAll()
+    }
+
+    @PutMapping("/{uid}")
+    fun actualizarUsuario(
+        @PathVariable uid: String,
+        @RequestBody usuarioActualizado: UsuarioEntity
+    ): ResponseEntity<UsuarioEntity> {
+        val usuarioExistente = IUsuarioRepository.findById(uid)
+
+        return if (usuarioExistente.isPresent) {
+            val usuario = usuarioExistente.get()
+
+            usuario.nombre = usuarioActualizado.nombre
+            usuario.email = usuarioActualizado.email
+
+            val usuarioGuardado = IUsuarioRepository.save(usuario)
+            ResponseEntity.ok(usuarioGuardado)
+        } else {
+            ResponseEntity.notFound().build()
+        }
     }
 }
