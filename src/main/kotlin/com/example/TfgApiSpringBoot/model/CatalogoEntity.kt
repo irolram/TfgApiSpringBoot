78cb890eb6bfbcca73821169ca0f1c9d8139adc0
irolram <ivanr9016@gmail.com>
2026-03-26 10:51:00 +0100

    Actu

--- src/main/kotlin/com/example/TfgApiSpringBoot/model/CatalogoEntity.kt
+++ src/main/kotlin/com/example/TfgApiSpringBoot/model/CatalogoEntity.kt
@@ -4,21 +4,37 @@ package com.example.TfgApiSpringBoot.model
 import jakarta.persistence.*
 
 @Entity
-@Table(name = "catalogo_plantas")
+@Table(name = "catalogo") 
 class CatalogoEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
 
     val nombre: String = "",
+
+    @Column(name = "nombre_cientifico")
     val nombreCientifico: String? = null,
+
     val riego: String? = null,
+
+    @Column(name = "luz_solar")
     val luzSolar: String? = null,
+
+    @Column(name = "icono_url")
     val iconoUrl: String? = null,
+
     @Column(columnDefinition = "TEXT")
-    val instrucciones: String,
-    val diasCrecimiento: Int,
-    val temporadaIdeal: String,
-    val profundidadSiembra: String,
-    val distanciaEntrePlantas: String
+    val instrucciones: String? = null,
+
+    @Column(name = "dias_crecimiento")
+    val diasCrecimiento: Int? = null,
+
+    @Column(name = "temporada_ideal")
+    val temporadaIdeal: String? = null,
+
+    @Column(name = "profundidad_siembra")
+    val profundidadSiembra: String? = null,
+
+    @Column(name = "distancia_entre_plantas")
+    val distanciaEntrePlantas: String? = null
 )
