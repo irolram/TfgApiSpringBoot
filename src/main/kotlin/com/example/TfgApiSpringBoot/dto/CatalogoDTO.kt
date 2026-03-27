0927606b05fb6045c32cc8552251bd19b64cb452
irolram <ivanr9016@gmail.com>
2026-03-27 12:07:38 +0100

    Actualizacion catalogo

--- src/main/kotlin/com/example/TfgApiSpringBoot/dto/CatalogoDTO.kt
+++ src/main/kotlin/com/example/TfgApiSpringBoot/dto/CatalogoDTO.kt
@@ -1,16 +1,26 @@
 package com.example.TfgApiSpringBoot.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 
 data class CatalogoDTO(
     val id: Long?,
+
+    @JsonProperty("nombre_cientifico") 
     val nombreCientifico: String?,
     val nombre: String?,
     val instrucciones: String?,
+    @JsonProperty("dias_crecimiento")
     val diasCrecimiento: Int?,
+    @JsonProperty("temporada_ideal")
     val temporadaIdeal: String?,
+    @JsonProperty("profundidad_siembra")
     val profundidadSiembra: String?,
+    @JsonProperty("distancia_entre_plantas")
     val distanciaEntrePlantas: String?,
+    @JsonProperty("icono_url")
     val icono: String?,
     val riego: String? = null,
+    @JsonProperty("luz_solar")
     val luzSolar: String? = null,
 )
