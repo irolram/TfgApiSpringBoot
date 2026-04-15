a24527e61d4c5840975a5fa6400449d943ad4a09
irolram <ivanr9016@gmail.com>
2026-04-15 09:36:11 +0200

    xdd

--- src/main/kotlin/com/example/TfgApiSpringBoot/controller/TicketController.kt
+++ src/main/kotlin/com/example/TfgApiSpringBoot/controller/TicketController.kt
@@ -14,7 +14,7 @@ class TicketController(private val ticketRepository: ITicketRepository) {
     @PostMapping
     fun crearTicket(@RequestBody ticket: TicketEntity): ResponseEntity<TicketEntity> {
         val guardado = ticketRepository.save(ticket)
-        return ResponseEntity.ok(guardado) 
+        return ResponseEntity.ok(guardado)
     }
 
     // 2. MOD/ADMIN: Listar solo los tickets abiertos
@@ -23,15 +23,13 @@ class TicketController(private val ticketRepository: ITicketRepository) {
         return ticketRepository.findByEstadoOrderByFechaDesc(EstadoTicket.ABIERTO)
     }
 
-    // 3. MOD/ADMIN: Resolver (Cerrar) un ticket
-    @PutMapping("/{id}/resolver")
-    fun resolverTicket(@PathVariable id: String): ResponseEntity<String> {
-        val ticket = ticketRepository.findById(id).orElse(null)
-            ?: return ResponseEntity.notFound().build()
+    @PutMapping("/{id}") 
+    fun resolverTicket(@PathVariable id: String): ResponseEntity<TicketEntity> {
+        val ticket = ticketRepository.findById(id).orElseThrow { Exception("Ticket no encontrado") }
 
-        ticket.estado = EstadoTicket.CERRADO
-        ticketRepository.save(ticket)
+        ticket.estado = EstadoTicket.CERRADO // Cambiamos el estado
+        val ticketGuardado = ticketRepository.save(ticket)
 
-        return ResponseEntity.ok("Ticket marcado como resuelto")
+        return ResponseEntity.ok(ticketGuardado)
     }
 }
